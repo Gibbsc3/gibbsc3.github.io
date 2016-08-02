5f315f7e0805a592897750a107d60207d0711d95
Tony <gibbsc@indiana.edu>
2016-08-02 18:21:22 -0400

    fresh

--- rtphotos/js/main.js
+++ rtphotos/js/main.js
@@ -5,13 +5,20 @@ var pusher,
 
     function renderPhoto(fake) {
       //creates image tag
-      console.log(fake.image);
       var img = document.createElement('img')
-      img.setAttribute('src', 'https://5f36d607.ngrok.io/photos' + fake.image)
+      img.setAttribute('src', fake.image)
+      img.classList.add('img-thumbnail')
+
+      var captionText = document.createTextNode(fake.caption)
+      var caption = document.createElement('h6')
+      caption.classList.add('text-center')
+      caption.appendChild(captionText)
       //creates div
       var div = document.createElement('div')
       div.classList.add('foo')
+      div.classList.add('col-xs-3')
       div.appendChild(img)
+      div.appendChild(caption)
       document.body.appendChild(div);
     }
 // Pusher connection
@@ -24,7 +31,7 @@ channel = pusher.subscribe('email_image')
 channel.bind('image_upload', function(data) {
 
 console.log(data);
-// renderPhoto(data)
+ renderPhoto(data)
   // Put your code here to render incoming photos
 })
 
@@ -39,13 +46,18 @@ console.log(data);
 
 
 // renderPhoto(fakePhotos)
-fetch('https://5f36d607.ngrok.io/photos')
+fetch('https://shrouded-hamlet-68398.herokuapp.com/photos')
   .then(function(data) {
     return data.json()
   })
   .then(function(photos) {
-    console.log(photos);
-    // renderPhoto(photos)
+
+    photos.forEach(function(photo){
+
+      console.log(photo.image);
+         renderPhoto(photo)
+    })
+
     // Put your code here to render existing photos
 
   })
